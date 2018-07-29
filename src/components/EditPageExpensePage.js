b69eefff53c979a83a405c436176bb6f6cee9568
Fay Vor <phavorsparks@gmail.com>
2018-07-29 04:43:30 +0100

    Update edit page

--- src/components/EditPageExpensePage.js
+++ src/components/EditPageExpensePage.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-const EditPageExpensePage = props => {
-  console.log(props);
-  return <div>Editing the expense with an id of {props.match.params.id}</div>;
-};
+const EditPageExpensePage = props => <div>Editing the expense with an id of {props.match.params.id}</div>;
+;
 
 export default EditPageExpensePage;
