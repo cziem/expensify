b3933794bf394baba26cf1db4ac4f17976ca2bc6
Fay Vor <phavorsparks@gmail.com>
2018-07-31 23:23:45 +0100

    setup provider from react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
 import AppRouter from "./routers/AppRouter";
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
@@ -24,5 +25,10 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById("app"));
