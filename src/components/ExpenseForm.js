c91d7f37917d33564fd9ce4c005df503c0ea1fb0
Fay Vor <phavorsparks@gmail.com>
2018-08-01 15:46:20 +0100

    create state management for ExpenseForm

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 
 export default class ExpenseForm extends Component {
+  state = {
+    description: '',
+    note: '',
+    amount: ''
+  }
+
+  onDescriptionChange = (e) => {
+    const description = e.target.value
+    this.setState(() => ({ description }))
+  }
+
+  onTextareaChange = (e) => {
+    const note = e.target.value
+    this.setState(() => ({ note }))
+  }
+
+  onAmountChange = (e) => {
+    const amount = e.target.value
+  if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }))
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Expense Form</h2>
+        <form>
+          <input
+            type="text"
+            placeholder='Description'
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="text"
+            placeholder='Amount'
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <textarea
+            placeholder='Add a note for your expense (optional)'
+            value={this.state.note}
+            onChange={this.onTextareaChange}
+          ></textarea>
+          <button>Add Expense</button>
+        </form>
       </div>
     )
   }
