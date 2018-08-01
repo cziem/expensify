abbe1c29d2c2de2fe3abc3e6dfdfa825dbbf327b
Fay Vor <phavorsparks@gmail.com>
2018-08-01 15:02:12 +0100

    Use ExpenseForm in AddExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ExpenseForm from './ExpenseForm';
 
-const AddExpensePage = () => <div>Add some expenses here</div>;
+const AddExpensePage = () => (
+  <div>
+    <h2>Add Expense</h2>
+    <ExpenseForm />
+  </div>
+)
 
 export default AddExpensePage;
