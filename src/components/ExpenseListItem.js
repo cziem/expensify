c9a75b014ff803aba46c338efd14db12e2b79553
Fay Vor <phavorsparks@gmail.com>
2018-09-27 18:29:57 +0100

    App Restructuring for the remove expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
     <p> {amount} - {createdAt}</p>
-    <button onClick={() => dispatch(removeExpense({ id }))}
-    >remove
-    </button>
   </div>
 )
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
